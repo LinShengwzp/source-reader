1d174a533eb6a0bbbf5c856000e8afca7cdb30e5
wzp12 <m18279625528@163.com>
2023-05-17 21:02:56 +0800

    引入 element plus 和图标

--- rust/src/components/Greet.vue
+++ rust/src/components/Greet.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import {Edit} from "@element-plus/icons-vue";
 
 const greetMsg = ref("");
 const name = ref("");
@@ -15,6 +16,7 @@ async function greet() {
   <div class="card">
     <input id="greet-input" v-model="name" placeholder="Enter a name..." />
     <button type="button" @click="greet()">Greet</button>
+    <el-button type="primary" :icon="Edit as any" circle />
   </div>
 
   <p>{{ greetMsg }}</p>
--- rust/src/main.ts
+++ rust/src/main.ts
@@ -1,5 +1,16 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import "./styles.css";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from "./App.vue";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App).mount("#app");
+
+let app = createApp(App);
+app.use(ElementPlus)
+// 引入 element 图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.mount("#app");
